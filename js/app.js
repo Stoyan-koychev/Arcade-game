d57942d0d758b6db347befc4ea173e3614a5b39b
Stoyan Koychev <stoqnkoi4ev@abv.bg>
2018-03-24 15:28:39 +0200

    feat: add user move

--- js/app.js
+++ js/app.js
@@ -38,13 +38,22 @@ Player.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Player.prototype.handleInput = function() {
-
+Player.prototype.handleInput = function(key) {
+  if (key == 'up' && this.y > 0) {
+    this.y -= 83;
+  } else if (key == 'down' && this.y < 404) {
+    this.y += 83;
+  } else if (key == 'left' && this.x > 0) {
+    this.x -= 101;
+  } else if (key == 'right' && this.x < 404) {
+    this.x += 101;
+  }
 };
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-let player = new Player();
+let player = new Player(202, 404);
+let allEnemies = [];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -55,6 +64,5 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
--- js/engine.js
+++ js/engine.js
@@ -27,8 +27,6 @@ var Engine = (function(global) {
     canvas.width = 505;
     canvas.height = 606;
     doc.body.appendChild(canvas);
-    let allEnemies = [];
-    let player = new Player(202, 404);
 
     /* This function serves as the kickoff point for the game loop itself
      * and handles properly calling the update and render methods.
